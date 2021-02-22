a9968c847f9d202e10bd0dc1402202536a5bb5c3
GnanaPradeepRS <gnana.gundluru@riversand.com>
2021-02-22 17:19:30 +0530

    added api to get linkedin count from an external python library

---
+++ api.py
@@ -0,0 +1,17 @@
+from linkedin_api import Linkedin
+from flask import request , Flask , jsonify
+
+app = Flask(__name__)
+
+##
+##
+##
+##print()
+
+@app.route('/externalapihandler/socialhandle' ,methods=['POST'])
+def create_task():
+    username = request.json.get('linkedin')
+    auth = Linkedin('cruz.thepal@gmail.com', 'iwbah@77')
+    profile = auth.get_company('riversand')
+    return jsonify({"count" : profile.get('followingInfo').get('followerCount')})
+app.run()
