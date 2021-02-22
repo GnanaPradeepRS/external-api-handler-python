dd34da310379ad1f9c36394d1e74c8cb01043822
GnanaPradeepRS <gnana.gundluru@riversand.com>
2021-02-23 03:04:49 +0530

    in azure

--- app.py
+++ app.py
@@ -1,21 +1,6 @@
 from linkedin_api import Linkedin
 from flask import request, Flask, jsonify
 
-# app = Flask(__name__)
-# # print('entered234')
-# # @app.route('/externalapihandler/socialhandle' , methods=['POST'])
-# # def create_task():
-# #     print('task')
-
-# @app.route('/externalapihandler/socialhandle' ,  methods=['POST'])
-# def index():
-#     return "<h1>Welcome to our server !!</h1>"
-
-# if __name__ == '__main__':
-#     app.run(threaded=True, port=5000)
-
-from flask import Flask
-
 app = Flask(__name__)
 
 @app.route("/externalapihandler/socialhandle",  methods=['POST'])
@@ -23,9 +8,10 @@ def home_view():
     try:
         username = request.json.get('linkedin')
         auth = Linkedin('cruz.thepal@gmail.com', 'iwbah@77')
-        print(auth)
         profile = auth.get_company('riversand')
         print(profile)
         return jsonify({"count" : profile.get('followingInfo').get('followerCount')})
     except Exception as error :
         print(error)
+if __name__ == "__main__": 
+        app.run() 
