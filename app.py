57e6469dd95c38d2d2bec61f42a9a57c9cdea2b6
GnanaPradeepRS <gnana.gundluru@riversand.com>
2021-02-23 00:58:57 +0530

    still experimenting

--- app.py
+++ app.py
@@ -3,19 +3,19 @@ from flask import request, Flask, jsonify
 
 app = Flask(__name__)
 print('entered234')
-# @app.route('/externalapihandler/socialhandle' ,methods=['POST'])
-# def create_task():
-#     print('task')
-#     username = request.json.get('linkedin')
-#     auth = Linkedin('cruz.thepal@gmail.com', 'iwbah@77')
-#     profile = auth.get_company('riversand')
-#     print('return')
-#     return jsonify({"count" : profile.get('followingInfo').get('followerCount')})
+@app.route('/externalapihandler/socialhandle' ,methods=['POST'])
+def create_task():
+    print('task')
+    username = request.json.get('linkedin')
+    auth = Linkedin('cruz.thepal@gmail.com', 'iwbah@77')
+    profile = auth.get_company('riversand')
+    print('return')
+    return jsonify({"count" : profile.get('followingInfo').get('followerCount')})
 
 
-@app.route('/externalapihandler/socialhandle')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
+# @app.route('/externalapihandler/socialhandle')
+# def index():
+#     return "<h1>Welcome to our server !!</h1>"
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
