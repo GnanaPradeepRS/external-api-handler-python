6a71f9df831f86840aef297e531736398a1b9d4c
GnanaPradeepRS <gnana.gundluru@riversand.com>
2021-02-23 01:29:40 +0530

    working on it

--- app/main.py
+++ app/main.py
@@ -1,16 +1,11 @@
-# from linkedin_api import Linkedin
-# from flask import request, Flask, jsonify
+from linkedin_api import Linkedin
+from flask import request, Flask, jsonify
 
 # app = Flask(__name__)
 # # print('entered234')
 # # @app.route('/externalapihandler/socialhandle' , methods=['POST'])
 # # def create_task():
 # #     print('task')
-# #     username = request.json.get('linkedin')
-# #     auth = Linkedin('cruz.thepal@gmail.com', 'iwbah@77')
-# #     profile = auth.get_company('riversand')
-# #     print('return')
-# #     return jsonify({"count" : profile.get('followingInfo').get('followerCount')})
 
 # @app.route('/externalapihandler/socialhandle' ,  methods=['POST'])
 # def index():
@@ -19,10 +14,15 @@
 # if __name__ == '__main__':
 #     app.run(threaded=True, port=5000)
 
-from flask import Flask 
-  
-app = Flask(__name__) 
-  
-@app.route("/externalapihandler/socialhandle" ,  methods=['POST']) 
-def home_view(): 
-        return "<h1>Welcome to Geeks for Geeks</h1>"
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/externalapihandler/socialhandle",  methods=['POST'])
+def home_view():
+    username = request.json.get('linkedin')
+    auth = Linkedin('cruz.thepal@gmail.com', 'iwbah@77')
+    profile = auth.get_company('riversand')
+    print('return')
+    return jsonify({"count" : profile.get('followingInfo').get('followerCount')})
