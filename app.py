7c64451e37cdb575b7ca323ee34293177eabb1c1
GnanaPradeepRS <gnana.gundluru@riversand.com>
2021-02-23 03:09:02 +0530

    in azure

--- app.py
+++ app.py
@@ -7,11 +7,10 @@ app = Flask(__name__)
 def home_view():
     try:
         username = request.json.get('linkedin')
-        auth = Linkedin('cruz.thepal@gmail.com', 'iwbah@77')
-        profile = auth.get_company('riversand')
-        print(profile)
-        return jsonify({"count" : profile.get('followingInfo').get('followerCount')})
-    except Exception as error :
-        print(error)
+        # auth = Linkedin('cruz.thepal@gmail.com', 'iwbah@77')
+        # profile = auth.get_company('riversand')
+        # print(profile)
+        # return jsonify({"count" : profile.get('followingInfo').get('followerCount')})
+        return username
 if __name__ == "__main__": 
         app.run() 
