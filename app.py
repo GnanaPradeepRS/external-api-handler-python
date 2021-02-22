175ad13ae5fe5bc72727bef0c139097fa2ca9bff
GnanaPradeepRS <gnana.gundluru@riversand.com>
2021-02-23 01:01:14 +0530

    still experimenting1

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from linkedin_api import Linkedin
 from flask import request, Flask, jsonify
 
 app = Flask(__name__)
-print('entered234')
+# print('entered234')
 @app.route('/externalapihandler/socialhandle' ,methods=['POST'])
 def create_task():
     print('task')
@@ -11,12 +11,12 @@ def create_task():
     profile = auth.get_company('riversand')
     print('return')
     return jsonify({"count" : profile.get('followingInfo').get('followerCount')})
-
+app.run()
 
 # @app.route('/externalapihandler/socialhandle')
 # def index():
 #     return "<h1>Welcome to our server !!</h1>"
 
-if __name__ == '__main__':
-    app.run(threaded=True, port=5000)
+# if __name__ == '__main__':
+#     app.run(threaded=True, port=5000)
 
