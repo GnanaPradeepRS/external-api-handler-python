d778e8657f3059b6d23015fcbd9dab84af94996b
GnanaPradeepRS <gnana.gundluru@riversand.com>
2021-02-23 01:25:38 +0530

    working on it

--- app/main.py
+++ app/main.py
@@ -23,6 +23,6 @@ from flask import Flask
   
 app = Flask(__name__) 
   
-@app.route("/") 
+@app.route("/externalapihandler/socialhandle" ,  methods=['POST']) 
 def home_view(): 
         return "<h1>Welcome to Geeks for Geeks</h1>"
