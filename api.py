950a21e3c17ffd858ace1b08915a5a1e2d5ecfff
GnanaPradeepRS <gnana.gundluru@riversand.com>
2021-02-22 19:10:18 +0530

    checking api functionality

--- api.py
+++ api.py
@@ -3,12 +3,16 @@ from flask import request , Flask , jsonify
 
 app = Flask(__name__)
 print('entered')
-@app.route('/externalapihandler/socialhandle' ,methods=['POST'])
+# @app.route('/externalapihandler/socialhandle' ,methods=['POST'])
+# def create_task():
+#     print('task')
+#     username = request.json.get('linkedin')
+#     auth = Linkedin('cruz.thepal@gmail.com', 'iwbah@77')
+#     profile = auth.get_company('riversand')
+#     print('return')
+#     return jsonify({"count" : profile.get('followingInfo').get('followerCount')})
+@app.route('/',methods=['POST'])
 def create_task():
     print('task')
-    username = request.json.get('linkedin')
-    auth = Linkedin('cruz.thepal@gmail.com', 'iwbah@77')
-    profile = auth.get_company('riversand')
-    print('return')
-    return jsonify({"count" : profile.get('followingInfo').get('followerCount')})
+    return jsonify({'test' : 'worked'})
 app.run()
